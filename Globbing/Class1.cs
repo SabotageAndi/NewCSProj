dafa000f444053edd014e83264f93d449c605d93
Andreas Willich <andreas.willich@gmail.com>
2018-02-12 20:56:43 +0100

    add files

---
+++ Globbing/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Globbing
+{
+    public class Class1
+    {
+    }
+}
---
+++ NuGetPackage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NuGetPackage
+{
+    public class Class1
+    {
+    }
+}
